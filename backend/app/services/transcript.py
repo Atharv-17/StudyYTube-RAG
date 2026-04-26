4de3985fe6f46ee8b516a886692bee4a85488d58
Atharv-17 <atharvligade17@gmail.com>
2026-04-26 20:47:16 +0530

    restructured entire backend with required files

--- backend/app/services/transcript.py
+++ backend/app/services/transcript.py
@@ -1,4 +1,6 @@
 from urllib.parse import urlparse, parse_qs
+from youtube_transcript_api import YouTubeTranscriptApi
+from app.utils.helpers import format_timestamp
 
 def get_video_id(url):
     parsed = urlparse(url)
