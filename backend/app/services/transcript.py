47ec1550ed48ae0b99049d32b37dbfcd5ed94813
Atharv-17 <atharvligade17@gmail.com>
2026-04-26 21:08:09 +0530

    added fetching  transcript logic

--- backend/app/services/transcript.py
+++ backend/app/services/transcript.py
@@ -6,3 +6,22 @@ def get_video_id(url):
     parsed = urlparse(url)
     video_id = parse_qs(parsed.query)["v"][0]
     return video_id
+
+#automatically appends , no need to loop it , we have optimized using AI,
+# optimized using list comprehension - faster and more Pythonic 
+def get_transcript(video_id):
+
+    try:
+        transcripts=YouTubeTranscriptApi.get_transcript(video_id)
+        formatted_lines = [
+        f"[{format_timestamp(item['start'])}] {item['text']}"
+            for item in transcripts
+        ]
+        return "\n".join(formatted_lines)
+        
+    except Exception as e:
+        raise Exception(f"Could not fetch the transcript of the given url: {str(e)}")
+
+    
+
+    
