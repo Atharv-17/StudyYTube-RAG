97cfbc52dae6aa6ad3e9f0e9508d1e44a2126eb9
Atharv-17 <atharvligade17@gmail.com>
2026-06-06 22:09:26 +0530

    Successfully changes the embedding model

--- backend/app/services/embeddings.py
+++ backend/app/services/embeddings.py
@@ -1,7 +1,30 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.embeddings import Embeddings
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from huggingface_hub import InferenceClient
 import os
+
+#completely last minute change for including embedding model with API and not downloading it.
+class HFAPIEmbeddings(Embeddings):
+    def __init__(self, model, token):
+        self.client = InferenceClient(model=model, token=token)
+
+    def embed_documents(self, texts):
+        return self.client.feature_extraction(
+            ["passage: " + t for t in texts]
+        )
+
+    def embed_query(self, text):
+        return self.client.feature_extraction(
+            "query: " + text
+        )
+
+def get_embeddings():
+    return HFAPIEmbeddings(
+        model="BAAI/bge-small-en-v1.5",
+        token=os.getenv("HF_TOKEN")
+    )
 
 def split_transcript(transcript):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -11,12 +34,7 @@ def split_transcript(transcript):
     chunks = text_splitter.split_text(transcript)
     return chunks
 
-def get_embeddings():
-    embeddings=HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2",
-        huggingfacehub_api_token=os.getenv("HF_TOKEN")
-    )
-    return embeddings
+
 
 def create_vector_store(chunks, embeddings):
     
